473240a29b03c68c0c1dfb4e2f94621031573ff5
aswistak@linkpoint.ca <swistek.labs@gmail.com>
2020-10-28 00:06:20 -0700

    Added receive() function for receiving ether from failed funcitons if revert() is not possible.

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -13,27 +13,27 @@ contract SimpleBank {
     //
     
     /* Fill in the keyword. Hint: We want to protect our users balance from other contracts*/
-    mapping (address => uint) balances;
+    mapping (address => uint) private balances;
     
     /* Fill in the keyword. We want to create a getter function and allow contracts to be able to see if a user is enrolled.  */
-    mapping (address => bool) enrolled;
+    mapping (address => bool) public enrolled;
 
     /* Let's make sure everyone knows who owns the bank. Use the appropriate keyword for this*/
-    address owner;
+    address public owner;
     
     //
     // Events - publicize actions to external listeners
     //
     
     /* Add an argument for this event, an accountAddress */
-    event LogEnrolled();
+    event LogEnrolled(address accountAddress);
 
     /* Add 2 arguments for this event, an accountAddress and an amount */
-    event LogDepositMade();
+    event LogDepositMade(address accountAddress, uint amount); 
 
     /* Create an event called LogWithdrawal */
     /* Add 3 arguments for this event, an accountAddress, withdrawAmount and a newBalance */
-
+    event LogWithdrawal(address accountAddress, uint withdrawAmount, uint newBalacnce);
 
     //
     // Functions
@@ -42,6 +42,7 @@ contract SimpleBank {
     /* Use the appropriate global variable to get the sender of the transaction */
     constructor() public {
         /* Set the owner to the creator of this contract */
+        owner = msg.sender;
     }
 
     // Fallback function - Called if other functions don't match call or
@@ -49,8 +50,21 @@ contract SimpleBank {
     // Typically, called when invalid data is sent
     // Added so ether sent to this contract is reverted if the contract fails
     // otherwise, the sender's money is transferred to contract
-    fallback() external payable {
+
+    event Received(address sender, uint value);
+
+    fallback() 
+        external
+        payable
+    {
         revert();
+    }
+
+    receive()
+        external
+        payable 
+    {
+        emit Received(msg.sender, msg.value);
     }
 
     /// @notice Get balance
@@ -59,6 +73,7 @@ contract SimpleBank {
     // allows function to run locally/off blockchain
     function getBalance() public returns (uint) {
         /* Get the balance of the sender of this transaction */
+        return owner.balance;
     }
 
     /// @notice Enroll a customer with the bank
